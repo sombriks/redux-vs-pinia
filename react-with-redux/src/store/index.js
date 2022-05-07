107b861d1150494d06602d0822b347c0eecf15fb
sombriks <sombriks@gmail.com>
2022-05-06 22:54:20 -0300

    wiring react vesion

--- react-with-redux/src/store/index.js
+++ react-with-redux/src/store/index.js
@@ -1,1 +1,13 @@
-export const store = {}
+import { createStore, combineReducers } from "redux"
+
+const userReducer = (state = {}, action) => {
+  return state
+}
+const filterReducer = (state = {}, action) => {
+  return state
+}
+const productsReducer = (state = {}, action) => {
+  return state
+}
+
+export const store = createStore(combineReducers({ userReducer, filterReducer, productsReducer }))
